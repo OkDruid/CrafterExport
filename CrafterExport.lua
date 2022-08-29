38418bfff5a68c1eb9b4e7c43a3dcdb5b9ce0f67
Ok <20994642+OkDruid@users.noreply.github.com>
2022-08-29 17:17:38 +0100

    Update CrafterExport.lua

--- CrafterExport.lua
+++ CrafterExport.lua
@@ -1,11 +1,10 @@
 -- CrafterExport.lua
 local addon_name, CE = ...
-local Excluded = CE.ExcludedRecipes
-local toggle = true
+local ExcludedRecipes = CE.ExcludedRecipes
+local toggle = false
 
 -- Create Frame and Register events
 local CrafterExport = CreateFrame("Frame", "CrafterExport")
-CrafterExport:Hide()
 CrafterExport:RegisterEvent("TRADE_SKILL_SHOW")
 CrafterExport:RegisterEvent("CRAFT_SHOW")
 CrafterExport:RegisterEvent("TRADE_SKILL_CLOSE")
@@ -13,7 +12,7 @@ CrafterExport:RegisterEvent("CRAFT_CLOSE")
 
 -- Handle events
 CrafterExport:SetScript("OnEvent", function(self, event)
-
+  
   -- When the profession window is open
 	if (event == "TRADE_SKILL_SHOW" or event == "CRAFT_SHOW") then
 		  CrafterExport:RegisterEvent("TRADE_SKILL_UPDATE")
@@ -28,87 +27,75 @@ CrafterExport:SetScript("OnEvent", function(self, event)
 
   -- When a profession is changed or filtered
   if (event == "TRADE_SKILL_UPDATE" or event == "CRAFT_UPDATE") then
-      ExportRecipes(Recipes(Excluded), toggle) 
+      openCraftExport(toggle) 
   end
 
 end)
 
--- Create Export Button, Frame, ScrollFrame and EditBox then pass recipe text and visibility state
-function ExportRecipes(text, closed) 
-  local Profession = CraftFrame or TradeSkillFrame
-
-  if (Profession) then
-    -- Create CrafterExportButton
-    CrafterExport.button = CreateFrame("Button", "CrafterExportButton", Profession, "UIPanelButtonTemplate")
-      CrafterExportButton:SetPoint("BOTTOMLEFT", Profession, 10, 45)
-      CrafterExportButton:SetSize(340, 25)
-      CrafterExportButton:SetFrameLevel(10)
-      CrafterExportButton:SetScript("OnClick", function()
-        toggleExport(self)
-        CrafterExportText:SetFocus()
-      end)
-      CrafterExportButton:SetText("Open CrafterExport")
-
-    -- Create CrafterExportFrame
-    CrafterExport.frame = CreateFrame("Frame", "CrafterExportFrame", Profession, "DialogBoxFrame")
-      CrafterExportFrameButton:Hide()
-      CrafterExportFrame:SetSize(340, 456)
-      CrafterExportFrame:SetPoint("TOPRIGHT", Profession, 310, -12)
-      CrafterExportFrame:SetMovable(false)
-      CrafterExportFrame:SetClampedToScreen(true)
-      CrafterExportFrame:SetBackdrop({ 
-        bgFile = "Interface\\Tooltips\\UI-Tooltip-Background", 
-        edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border", 
-        edgeSize = 16, 
-        insets = { left = 4, right = 4, top = 4, bottom = 4 } 
-      })
-      CrafterExportFrame:SetBackdropBorderColor(255, 255, 255, 1)
-      CrafterExportFrame:SetBackdropColor(0.1, 0.1, 0.1, 1)
-      CrafterExportFrame:Hide()
-
-    -- Create CrafterExportScroll
-    CrafterExport.scroll = CreateFrame("ScrollFrame", "CrafterExportScroll", CrafterExport.frame, "UIPanelScrollFrameTemplate")
-      CrafterExportScroll:SetPoint("TOP", 0, -18)
-      CrafterExportScroll:SetPoint("BOTTOM", 0, 18)
-      CrafterExportScroll:SetPoint("LEFT", 18, 0)
-      CrafterExportScroll:SetPoint("RIGHT", -36, 0)
-
-    -- Create CrafterExportText
-    CrafterExport.text = CreateFrame("EditBox", "CrafterExportText",  CrafterExport.scroll)
-      CrafterExportText:SetSize(CrafterExportScroll:GetSize())
-      CrafterExportText:SetMultiLine(true)
-      CrafterExportText:SetAutoFocus(false)
-      CrafterExportText:SetFontObject("ChatFontSmall")
-      CrafterExportText:SetScript("OnEscapePressed", function()
-        CrafterExportFrame:Hide()
-        toggleExport(self)
-      end)
-      CrafterExportScroll:SetScrollChild(CrafterExportText)
-
-
-    if closed then
-      CrafterExportFrame:Hide()
-      CrafterExportButton:SetText("Open CrafterExport")
-      CrafterExportText:SetText("")
-    else
-      CrafterExportFrame:Show()
-      CrafterExportButton:SetText("Close CrafterExport")
-      CrafterExportText:SetText(text:sub(1, -2))
-      CrafterExportText:HighlightText()
-    end
-
-  end -- profession
-
-end
-
--- set CrafterExportFrame visibility and trigger on button click
-function toggleExport(self)
+function toggleCraftExport(self)
   if (toggle) then
     toggle = false
   else
     toggle = true
   end
-  ExportRecipes(Recipes(Excluded), toggle)
+  openCraftExport(toggle)
+end
+
+function openCraftExport(closed)
+  if not CrafterExportFrame then
+    createCrafterExport()
+  end
+
+  local recipes = Recipes(ExcludedRecipes);
+
+  CrafterExportText:SetText(recipes:sub(1, -2))
+  CrafterExportText:HighlightText()
+  CrafterExportFrameButton:Hide()
+
+  if (closed) then
+    CrafterExportFrame:Show()
+    CrafterExportButton:SetText("Close CrafterExport")
+  else
+    CrafterExportFrame:Hide()
+    CrafterExportButton:SetText("Open CrafterExport")
+  end
+
+end
+
+function createCrafterExport() 
+  local Profession = CraftFrame or TradeSkillFrame
+  local frame = CreateFrame("Frame", "CrafterExportFrame", Profession, "DialogBoxFrame")
+  frame:SetSize(340, 456)
+  frame:SetPoint("TOPRIGHT", Profession, 310, -12)
+  frame:SetMovable(false)
+  frame:SetClampedToScreen(true)
+  frame:SetBackdrop({ bgFile = "Interface\\Tooltips\\UI-Tooltip-Background", edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border", edgeSize = 16, insets = { left = 4, right = 4, top = 4, bottom = 4 } })
+  frame:SetBackdropBorderColor(255, 255, 255, 1)
+  frame:SetBackdropColor(0.1, 0.1, 0.1, 1)
+  frame:SetFrameStrata("HIGH")
+  
+  frame.scrollFrame = CreateFrame("ScrollFrame", "CrafterExportScroll", CrafterExportFrame, "UIPanelScrollFrameTemplate")
+  frame.scrollFrame:SetPoint("TOP", 0, -18)
+  frame.scrollFrame:SetPoint("BOTTOM", 0, 18)
+  frame.scrollFrame:SetPoint("LEFT", 18, 0)
+  frame.scrollFrame:SetPoint("RIGHT", -36, 0)
+  
+  frame.editBox = CreateFrame("EditBox", "CrafterExportText",  CrafterExportScroll)
+  frame.editBox:SetPoint("TOPLEFT", frame.scrollFrame, 5, -5)
+  frame.editBox:SetSize(CrafterExportScroll:GetSize())
+  frame.editBox:SetMultiLine(true)
+  frame.editBox:SetAutoFocus(false)
+  frame.editBox:SetFontObject("ChatFontSmall")
+  frame.editBox:SetMaxLetters(99999)
+  frame.editBox:SetScript("OnEscapePressed", function() frame:Hide() end)
+  frame.scrollFrame:SetScrollChild(frame.editBox)
+  
+
+  frame.button = CreateFrame("Button", "CrafterExportButton", Profession, "UIPanelButtonTemplate")
+  frame.button:SetPoint("BOTTOMLEFT", Profession, 10, 45)
+  frame.button:SetSize(340, 25)
+  frame.button:SetFrameLevel(10)
+  frame.button:SetScript("OnClick", function() toggleCraftExport(self) end)
 end
 
 -- get table of recipe names from open profession window
