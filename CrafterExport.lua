6d19ec5558c8136e8b65692b75f63f8c961c297e
Ok <20994642+OkDruid@users.noreply.github.com>
2022-08-30 03:02:13 +0100

    Update CrafterExport.lua

--- CrafterExport.lua
+++ CrafterExport.lua
@@ -3,6 +3,20 @@ local addon_name, CE = ...
 local ExcludedRecipes = CE.ExcludedRecipes
 local toggle = false
 
+-- profession text colors
+local professions = {
+  Alchemy = "0.64, 0.82, 0.79",
+  Blacksmithing = "0.51, 0.50, 0.51",
+  Cooking = "0.91, 0.56, 0.36",
+  Enchanting = "0.95, 0.36, 0.32",
+  Engineering = "1, 0.89, 0.33",
+  Inscription = "0.28, 0.53, 0.31",
+  Jewelcrafting = "0.56, 0.42, 0.58",
+  Leatherworking = "0.65, 0.5, 0.35",
+  Mining = "0.49, 0.49, 0.41",
+  Smelting = "0.49, 0.49, 0.41",
+  Tailoring = "0.82, 0.78, 0.69"
+}
 
 function OnEvent(self, event, ...)
 
@@ -43,11 +57,10 @@ function isTradeOrCraft()
   local craftName, craftRank, _ = GetCraftDisplaySkillLine()
   
   if (craftRank > 0) then
-    return CraftFrame;
+    return CraftFrame
+  else
+    return TradeSkillFrame
   end
-  
-  return TradeSkillFrame;
-  
 end
 
 function tablelength(T)
@@ -56,22 +69,32 @@ function tablelength(T)
   return count
 end
 
+function ProfessionTextColor(index)
+  if (index) then
+  CrafterExportText:SetTextColor(index:match("^%s*(.-)%s*$"):match("([^,]+),([^,]+),([^,]+)"))
+  end
+end
+
 function openCrafterExport(closed)
   if not CrafterExportFrame then
     createCrafterExport()
   end
-  local source = GetRecipes();
-  local recipes = Recipes(ExcludedRecipes, source);
+  
+  local localizedClass, englishClass, classIndex = UnitClass("player")
+  local source = GetRecipes()
+  local recipes = Recipes(ExcludedRecipes, source)
   local recipeCount =  tablelength(source)
   local craftName, craftRank, _ = GetCraftDisplaySkillLine()
 	local tsName, tsRank, _ = GetTradeSkillLine()
-  local professionName = tsName;
+  local professionName = tsName
   if (craftRank > 0) then
-    professionName = craftName;
+    professionName = craftName
   end
   CrafterExportFrame.title:SetText("CrafterExport: " .. professionName .. " (" .. recipeCount .. ")")
   CrafterExportText:SetText(recipes:sub(1, -2))
-
+  
+  ProfessionTextColor(professions[professionName])
+  
   if (closed) then
     CrafterExportFrame:Show()
     CrafterExportButton:SetText("Close CrafterExport")
@@ -84,43 +107,48 @@ end
 
 function createCrafterExport() 
   local Profession = isTradeOrCraft()
-  local frame = CreateFrame("Frame", "CrafterExportFrame", Profession, "BasicFrameTemplateWithInset")
-  frame:SetSize(300, 454)
-  frame:SetPoint("TOPRIGHT", Profession, 270, -13)
+  local frame = CreateFrame("Frame", "CrafterExportFrame", Profession, "TooltipBorderedFrameTemplate")
+  frame:SetPoint("TOPRIGHT", Profession, 270, -12)
+  frame:SetSize(300, 456)
+  frame:SetBackdropBorderColor(255, 255, 255, 1)
+  frame:SetBackdropColor(32/255, 34/255, 37/255)
   frame:SetMovable(false)
   frame:SetClampedToScreen(true)
   frame:SetFrameStrata("HIGH")
-  
+
   frame.title = frame:CreateFontString(nil, "OVERLAY")
-	frame.title:SetFontObject("Game13Font") --SystemFont_Outline_Small
-	frame.title:SetPoint("LEFT", frame.TitleBg, 5, 0)	
+	frame.title:SetFontObject("SystemFont_Outline")
+	frame.title:SetPoint("TOPLEFT", frame, 12, -10)	
 
   frame.scrollFrame = CreateFrame("ScrollFrame", "CrafterExportScroll", CrafterExportFrame, "UIPanelScrollFrameTemplate")
   frame.scrollFrame:SetPoint("TOP", 0, -32)
   frame.scrollFrame:SetPoint("BOTTOM", 0, 36)
   frame.scrollFrame:SetPoint("LEFT", 12, 0)
-  frame.scrollFrame:SetPoint("RIGHT", -36, 0)
+  frame.scrollFrame:SetPoint("RIGHT", -32, 0)
   
   frame.editBox = CreateFrame("EditBox", "CrafterExportText",  CrafterExportScroll)
   frame.editBox:SetPoint("TOPLEFT", frame.scrollFrame, 5, -5)
   frame.editBox:SetSize(frame.scrollFrame:GetSize())
   frame.editBox:SetMultiLine(true)
   frame.editBox:SetAutoFocus(false)
-  frame.editBox:SetFontObject("ChatFontSmall")
+  frame.editBox:SetFontObject("SystemFont_Outline_Small")
   frame.editBox:SetMaxLetters(99999)
-  frame.editBox:SetScript("OnEscapePressed", function() frame:Hide() end)
+  --frame.editBox:EnableKeyboard(false)
+  frame.editBox:SetScript("OnEscapePressed", function()
+    frame:Hide()
+  end)
   frame.scrollFrame:SetScrollChild(frame.editBox)
 
-  frame.select = CreateFrame("Button", nil, CrafterExportFrame, "UIPanelButtonTemplate");
-  frame.select:SetPoint("BOTTOM", 0, 7);
-  frame.select:SetPoint("LEFT", 6, 0);
-  frame.select:SetSize(286, 25);
-  frame.select:SetText("Select All");
-  frame.select:SetNormalFontObject("GameFontNormal");
-  frame.select:SetHighlightFontObject("GameFontHighlight");
+  frame.select = CreateFrame("Button", nil, CrafterExportFrame, "UIPanelButtonTemplate")
+  frame.select:SetPoint("BOTTOM", 0, 7)
+  frame.select:SetPoint("LEFT", 6, 0)
+  frame.select:SetSize(288, 25)
+  frame.select:SetText("Select All")
+  frame.select:SetNormalFontObject("GameFontNormal")
+  frame.select:SetHighlightFontObject("GameFontHighlight")
   frame.select:SetScript("OnClick", function() 
-    CrafterExportText:HighlightText()
-    CrafterExportText:SetFocus(true)
+    frame.editBox:HighlightText()
+    frame.editBox:SetFocus(true)
   end)
 
   frame.button = CreateFrame("Button", "CrafterExportButton", Profession, "UIPanelButtonTemplate")
@@ -128,6 +156,7 @@ function createCrafterExport()
   frame.button:SetSize(340, 25)
   frame.button:SetFrameLevel(10)
   frame.button:SetScript("OnClick", function() toggleCrafterExport(self) end)
+  
 end
 
 -- get table of recipe names from open profession window
